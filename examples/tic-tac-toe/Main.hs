906eb64015f908f0326720e5d3f75d675d8a88d1
Jürgen Peters <taulmarill@xgn.de>
2017-03-09 22:32:10 +0100

    win animation for tictactoe

--- examples/tic-tac-toe/Main.hs
+++ examples/tic-tac-toe/Main.hs
@@ -28,8 +28,43 @@ handle (MouseClick p) = do
   alreadyMarked <- marked p
   if alreadyMarked then nop else do
     createSprite p 1 spr
-    put $ nextPlayer state
+    won <- hasWon p
+    if won then changeStepper (winanim spr) >> changeHandler (triggerNewGame)
+           else put $ nextPlayer state
 handle _ = nop
+
+winanim :: String -> Step Player ()
+winanim player = do
+  fn <- readFrameNr
+  if mod fn 10 /= 0 then return () else do 
+    sq <- findFirstNot player
+    case sq of
+      Nothing  -> return ()
+      Just sq' -> removeAllSprites sq' >> createSprite sq' 1 player
+
+findFirstNot :: String -> Step a (Maybe (Int,Int))
+findFirstNot player = do
+  let allSqares = (,) <$> [0..2] <*> [0..2] 
+  allSprites <- zip allSqares <$> mapM findSpriteByXY allSqares
+  let allNonPlayerSquares = filter (not. elem player . snd) allSprites
+  case allNonPlayerSquares of
+    (s:_) -> return $ Just $ fst s
+    []    -> return Nothing
+
+triggerNewGame :: Handle Player ()
+triggerNewGame (MouseClick _) = resetGame
+triggerNewGame (KeyPress   _) = resetGame
+triggerNewGame _ = nop
+
+resetGame :: Step Player ()
+resetGame = do
+  mapM removeAllSprites $ (,) <$> [0..2] <*> [0..2] 
+  changeStepper nop
+  changeHandler handle
+  put PlayerX
+
+removeAllSprites :: (Int,Int) -> Step Player ()
+removeAllSprites sq = findSpriteByXY sq >>= mapM_ (deleteSprite sq)
 
 marked :: (Int,Int) -> Step Player Bool
 marked p = findSpriteByXY p >>= (return . ( /= []))
@@ -42,3 +77,19 @@ nextPlayer :: Player -> Player
 nextPlayer PlayerX = PlayerO
 nextPlayer PlayerO = PlayerX
 
+hasWon :: (Int,Int) -> Step Player Bool
+hasWon p = do
+  player <- playerSprite <$> get
+  let lastPlay  = filter (elem p) possibleWins
+      lastPlay' = map (filter (not . (== p))) lastPlay
+  any id <$> mapM (checkWin player) lastPlay'
+
+checkWin :: String -> [(Int,Int)] -> Step a Bool
+checkWin player ps = all id <$> mapM (\p -> elem player <$> findSpriteByXY p) ps
+
+possibleWins :: [[(Int,Int)]]
+possibleWins = 
+  [ [(0,0),(1,0),(2,0)], [(0,1),(1,1),(2,1)], [(0,2),(1,2),(2,2)]
+  , [(0,0),(0,1),(0,2)], [(1,0),(1,1),(1,2)], [(2,0),(2,1),(2,2)]
+  , [(0,0),(1,1),(2,2)], [(2,0),(1,1),(0,2)]
+  ]
