91eb73513d1075545a2049dc8109f311014a3752
Jürgen Peters <taulmarill@xgn.de>
2017-03-16 17:23:08 +0100

    updated tictactoe example

--- examples/tic-tac-toe/Main.hs
+++ examples/tic-tac-toe/Main.hs
@@ -1,20 +1,20 @@
-module Main where 
+module Main where
 
 import LambdaLudo
 
-conf = Config 
+conf = Config
   { stepper     = nop
   , handler     = handle
   , initializer = initGame
   , memory      = PlayerX
-  , assets      = ["Background.png", "X.png", "O.png"]
+  , assets      = ["Background.png", "X.png", "O.png", "Marker.png"]
   , columns     = 3
   , rows        = 3
   , size        = 300
   }
 
 data Player = PlayerX | PlayerO
-  
+
 main :: IO ()
 main = runGame conf
 
@@ -23,51 +23,24 @@ initGame = setBackgroundImage "Background"
 
 handle :: Handle Player ()
 handle (MouseClick p) = do
-  state <- get
-  let spr = playerSprite state
+  player <- get
+  let image = playerSprite player
   alreadyMarked <- marked p
   if alreadyMarked then nop else do
-    createSprite p 1 spr
+    createSprite p 1 image
     won <- hasWon p
-    if won then changeStepper (winanim spr) >> changeHandler (triggerNewGame)
-           else put $ nextPlayer state
+    if won == [] then nop else do
+      mapM_ (\x -> createSprite x 2 "Marker") won
+      changeHandler triggerNewGame
+    draw <- checkDraw p
+    if draw then changeHandler triggerNewGame else nop
+    put (nextPlayer player)
 handle _ = nop
 
-winanim :: String -> Step Player ()
-winanim player = do
-  fn <- readFrameNr
-  if mod fn 10 /= 0 then return () else do 
-    sq <- findFirstNot player
-    case sq of
-      Nothing  -> return ()
-      Just sq' -> removeAllSprites sq' >> createSprite sq' 1 player
-
-findFirstNot :: String -> Step a (Maybe (Int,Int))
-findFirstNot player = do
-  let allSqares = (,) <$> [0..2] <*> [0..2] 
-  allSprites <- zip allSqares <$> mapM findSpriteByXY allSqares
-  let allNonPlayerSquares = filter (not. elem player . snd) allSprites
-  case allNonPlayerSquares of
-    (s:_) -> return $ Just $ fst s
-    []    -> return Nothing
-
-triggerNewGame :: Handle Player ()
-triggerNewGame (MouseClick _) = resetGame
-triggerNewGame (KeyPress   _) = resetGame
-triggerNewGame _ = nop
-
-resetGame :: Step Player ()
-resetGame = do
-  mapM removeAllSprites $ (,) <$> [0..2] <*> [0..2] 
-  changeStepper nop
-  changeHandler handle
-  put PlayerX
-
-removeAllSprites :: (Int,Int) -> Step Player ()
-removeAllSprites sq = findSpriteByXY sq >>= mapM_ (deleteSprite sq)
-
 marked :: (Int,Int) -> Step Player Bool
-marked p = findSpriteByXY p >>= (return . ( /= []))
+marked square = do
+  sprites <- findSpriteByXY square
+  return (sprites /= [])
 
 playerSprite :: Player -> String
 playerSprite PlayerX = "X"
@@ -77,19 +50,45 @@ nextPlayer :: Player -> Player
 nextPlayer PlayerX = PlayerO
 nextPlayer PlayerO = PlayerX
 
-hasWon :: (Int,Int) -> Step Player Bool
-hasWon p = do
-  player <- playerSprite <$> get
-  let lastPlay  = filter (elem p) possibleWins
-      lastPlay' = map (filter (not . (== p))) lastPlay
-  any id <$> mapM (checkWin player) lastPlay'
-
-checkWin :: String -> [(Int,Int)] -> Step a Bool
-checkWin player ps = all id <$> mapM (\p -> elem player <$> findSpriteByXY p) ps
-
 possibleWins :: [[(Int,Int)]]
-possibleWins = 
+possibleWins =
   [ [(0,0),(1,0),(2,0)], [(0,1),(1,1),(2,1)], [(0,2),(1,2),(2,2)]
   , [(0,0),(0,1),(0,2)], [(1,0),(1,1),(1,2)], [(2,0),(2,1),(2,2)]
   , [(0,0),(1,1),(2,2)], [(2,0),(1,1),(0,2)]
   ]
+
+checkWin :: [(Int,Int)] -> Step Player [(Int,Int)]
+checkWin w = do
+  marks <- mapM findSpriteByXY w
+  player <- get
+  let p = playerSprite player
+  return (if marks == [[p],[p]] then w else [])
+
+hasWon :: (Int,Int) -> Step Player [(Int,Int)]
+hasWon p = do
+  w <- mapM (checkWin . filter (/= p)) possibleWins
+  let wins = filter (/= []) w
+  return (if wins == [] then [] else p : head wins)
+
+allSquares :: [(Int,Int)]
+allSquares = [(0,0),(1,0),(2,0),(0,1),(1,1),(2,1),(0,2),(1,2),(2,2)]
+
+removeAllSprites :: (Int,Int) -> Step Player ()
+removeAllSprites sq = do
+  s <- findSpriteByXY sq
+  mapM_ (deleteSprite sq) s
+
+resetGame :: Step Player ()
+resetGame = do
+  mapM removeAllSprites allSquares
+  changeHandler handle
+  put PlayerX
+
+triggerNewGame :: Handle Player ()
+triggerNewGame (MouseClick _) = resetGame
+triggerNewGame _ = nop
+
+checkDraw :: (Int,Int) -> Step Player Bool
+checkDraw p = do
+  sq <- mapM findSpriteByXY (filter (/= p) allSquares)
+  return (filter (== []) sq == [])
